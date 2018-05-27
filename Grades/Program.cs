df8a682badaf06e0c9ad0ef5df54e840443411e2
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-05-27 21:31:39 +0200

    point variables  book and book 2 to the same object

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -15,7 +15,7 @@ namespace Grades
             book.AddGrade(91);
             book.AddGrade(89.5f);
 
-            GradeBook book2 = new GradeBook();
+            GradeBook book2 = book;
             book2.AddGrade(75);
 
         }
