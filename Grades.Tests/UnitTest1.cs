db99a8840500efd612f20b57f6a3ab4f25019069
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-02 21:20:01 +0200

    Add unit test project

---
+++ Grades.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void CalculateHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            {
+                book.AddGrade(90f);
+                book.AddGrade(89.1f);
+
+                GradeStatistics stats = book.ComputeStatistics();
+                Assert.AreEqual(90f, stats.HighestGrade);
+                Assert.AreEqual(89.1f, stats.LowestGrade);
+            }
+        }
+
+            [TestMethod]
+        public void PassByValue()
+        {
+            GradeBook book = new GradeBook();
+            book.Name = "Not set";
+            SetName(book);
+            Assert.AreEqual("Name set", book.Name);
+
+
+        }
+        void SetName(GradeBook book)
+        {
+            book.Name = "Name set";
+        }
+    }
+}
+
