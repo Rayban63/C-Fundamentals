c14f85f3d815c05c256e46e056cee69a702b624c
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-02 17:53:42 +0200

    show behavior of value types like an int

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -8,14 +8,28 @@ namespace Grades
 {
     class Program
     {
+        static void GiveBookAName(GradeBook book)
+        {
+            book.Name = "The Gradebook";
+        }
+
+        static void IncrementANumber(int number)
+        {
+            number += 1;
+        }
         static void Main(string[] args)
         {
             GradeBook g1 = new GradeBook(); // make new instance of the gradebook
             GradeBook g2 = g1; //set reference type g2 to be equal to g1
 
-            g1 = new GradeBook(); // create new reference type g1 
-            g1.Name = "Scott's Gradebook"; // Set the name of g1
-            Console.WriteLine(g2.Name);// console output is empty, because g2 is still pointing towards the first referance of g1
+            GiveBookAName(g2);// invoke GiveBookAName
+           
+            Console.WriteLine(g1.Name);/* console output will be The Gradebook, because all the references
+            (g1, g2 and book) are pointing towards the object of Gradebook*/
+
+            int x1 = 4;
+            IncrementANumber(x1);
+            Console.WriteLine(x1); // console output will remain 4 because the methode IncrementANumber can't change x1
 
           
 
