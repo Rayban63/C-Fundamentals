e3e5d47ef9ff871348f9e24e1ab4bf61042e9c42
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-03 21:15:56 +0200

    Add If Else statement to assign a lettergrade

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -13,8 +13,42 @@ namespace Grades
             HighestGrade = 0; // initialized to 0, when a new high grade is entered HighestGrade will be overwritten by the foreach loop in gradebook
             LowestGrade = float.MaxValue; // initialized to MaxValue, when a new Low grade is entered LowestGrade will be overwritten by the foreach loop in gradebook
         }
-       public float AverageGrade;
-       public float HighestGrade;
-       public float LowestGrade;
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (AverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if (AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if (AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if(AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+
+                return result;
+
+            }
+
+            
+        }
+
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -13,18 +13,19 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook("Ray's Gradebook");
-            book.Name = " Bill's Gradebook";
-            WriteNames(book.Name);
+           
 
             book.AddGrade(91);
             book.AddGrade(89.1f);
             book.AddGrade(75f);
+            
 
             GradeStatistics stats = book.ComputeStatistics();
 
             Console.WriteLine(stats.AverageGrade);
             Console.WriteLine(stats.LowestGrade);
             Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LetterGrade);
 
         }
 
