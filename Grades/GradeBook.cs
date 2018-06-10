0a87e3bfeeed0fa1619db0ca3e0d5d86f0b5d75a
RayDev <rvdz_@DESKTOP-O0SEM5T>
2018-06-10 12:17:53 +0200

    add itaration

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,31 +13,44 @@ namespace Grades
         public GradeBook(string name = "There is no name")// this is the default value when no name is passed in.
         {
             Name = name;
-            grades = new List<float>();
+            _grades = new List<float>();
 
         }
         public void AddGrade(float grade)
         {
             if (grade >= 0 && grade <= 100)
             {
-                grades.Add(grade);
+                _grades.Add(grade);
 
             }
            
         }
+
+        public void WriteGrades(TextWriter textWriter)
+            
+        {
+            textWriter.WriteLine("Grades: ");
+            for (int i = 0; i < _grades.Count; i++)
+            {
+                textWriter.WriteLine(_grades[i]);
+
+            }
+            textWriter.WriteLine("******** ");
+        }
+
         public GradeStatistics ComputeStatistics()
         {
             GradeStatistics stats = new GradeStatistics();
             float sum = 0f; //this variable is to calculate the total ammount of grades
 
-            foreach (float grade in grades)
+            foreach (float grade in _grades)
             {
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade); //overwrite initial grade in GradeStatistics when grade is higher
                 stats.LowestGrade = Math.Min(grade, stats.LowestGrade); //overwrite initial grade in GradeStatistics when grade is lower
                 sum += grade;
 
             }
-            stats.AverageGrade = sum / grades.Count; //Calculate the average of the sum and add them to Averagegrade
+            stats.AverageGrade = sum / _grades.Count; //Calculate the average of the sum and add them to Averagegrade
             return stats;
         }
 
@@ -66,7 +80,7 @@ namespace Grades
             }
         }
         public event NameChangedDelegate NameChanged; // by adding the keyword event this delegate is promoted to an event
-       private List<float> grades; //grades are made private so the can only be update via the methode AddGrade
+       private List<float> _grades; //grades are made private so the can only be update via the methode AddGrade
 
 
     }
